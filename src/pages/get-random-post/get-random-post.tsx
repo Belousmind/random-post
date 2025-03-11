3fd1f4e55d3b0928984ab4ae528efee2c7413907
belousmind <belousmind@gmail.com>
2025-03-11 19:34:34 +0300

    feat: add logic of post loading

--- src/pages/get-random-post/get-random-post.tsx
+++ src/pages/get-random-post/get-random-post.tsx
@@ -1,11 +1,38 @@
+import { useState } from "react";
+import { fetchRandomPost, Post } from "../../shared/api/fetch-posts";
+import { getRandomNumber } from "../../shared/helpers/get-random-number";
 
 const GetRandomPostPage = () => {
+  const [post, setPost] = useState<Post | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleGetPost = async () => {
+    setLoading(true);
+    try {
+      const postId = getRandomNumber(100);
+      const data = await fetchRandomPost(postId);
+      setPost(data);
+    } catch (error) {
+      console.error("Ошибка при загрузке поста:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <>
-      <h1>Рандомный пост</h1>
-      <button>Получить</button>
-    </>
-  )
-}
+    <div>
+      <h1>Случайный пост</h1>
+      <button onClick={handleGetPost} disabled={loading}>
+        {loading ? "Загрузка..." : "Получить"}
+      </button>
+
+      {post && (
+        <div>
+          <h2>{post.title}</h2>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default GetRandomPostPage;
