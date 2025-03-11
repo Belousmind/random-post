0d55ff107165754706218c4e43b6762002b2da46
belousmind <belousmind@gmail.com>
2025-03-11 18:57:02 +0300

    feat: add pages routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ROUTES } from "../shared/services/routes";
+import Header from "../shared/UI/header";
+import GetRandomPostPage from "../pages/get-random-post/get-random-post";
+import Main from "../pages/main/main";
+
 
 function App() {
 
   return (
     <>
-
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path={ROUTES.HOME} element={<Main/>} />
+          <Route path={ROUTES.POST} element={<GetRandomPostPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/pages/get-random-post/get-random-post.tsx
@@ -0,0 +1,11 @@
+
+const GetRandomPostPage = () => {
+  return (
+    <>
+      <h1>Рандомный пост</h1>
+      <button>Получить</button>
+    </>
+  )
+}
+
+export default GetRandomPostPage;
