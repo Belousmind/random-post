e2464cf8ca51c73ca4307a016a55571fc1300c31
belousmind <belousmind@gmail.com>
2025-03-17 18:17:32 +0300

    feat: add route from / to /landing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,12 @@
 import Landing from "@pages/landing/landing";
 import "./App.scss";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/landing" replace />} />
         <Route path="/landing" element={<Landing />}></Route>
       </Routes>
     </BrowserRouter>
