8e36a05ddbd1a0553d26e77f35d46ead8e8fc83f
belousmind <belousmind@gmail.com>
2025-03-29 11:05:05 +0300

    feat: setup linting and formatting tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import './App.css'
 
-const USER_READ_PERMISSIONS = [
-	'vacancies',
-	'users',
-	'candidates',
-	'clients',
-	'partners',
-]
+const USER_READ_PERMISSIONS = ['vacancies', 'users', 'candidates', 'clients', 'partners']
 
 const checkHasUserPermission = (routeName) => {
-	return USER_READ_PERMISSIONS.includes(routeName)
+  return USER_READ_PERMISSIONS.includes(routeName)
 }
 
 // Со звездочкой проверка прав асинхронная
@@ -18,116 +12,113 @@ const checkHasUserPermission = (routeName) => {
 // }
 
 const routes = {
-	vacancies: {
-		name: 'vacancies',
-		pathname: 'vacancies',
-		getLink: () => '/vacancies',
-		text: 'Вакансии',
-	},
-	candidates: {
-		name: 'candidates',
-		pathname: 'candidates',
-		getLink: () => '/candidates',
-		text: 'Кандидаты',
-	},
-	events: {
-		name: 'events',
-		pathname: 'events',
-		getLink: () => '/events',
-		text: 'События',
-	},
-	clients: {
-		name: 'clients',
-		pathname: 'clients',
-		getLink: () => '/clients',
-		text: 'Клиенты',
-	},
-	partners: {
-		name: 'partners',
-		pathname: 'partners',
-		getLink: () => '/partners',
-		text: 'Клиенты',
-	},
+  vacancies: {
+    name: 'vacancies',
+    pathname: 'vacancies',
+    getLink: () => '/vacancies',
+    text: 'Вакансии',
+  },
+  candidates: {
+    name: 'candidates',
+    pathname: 'candidates',
+    getLink: () => '/candidates',
+    text: 'Кандидаты',
+  },
+  events: {
+    name: 'events',
+    pathname: 'events',
+    getLink: () => '/events',
+    text: 'События',
+  },
+  clients: {
+    name: 'clients',
+    pathname: 'clients',
+    getLink: () => '/clients',
+    text: 'Клиенты',
+  },
+  partners: {
+    name: 'partners',
+    pathname: 'partners',
+    getLink: () => '/partners',
+    text: 'Клиенты',
+  },
 }
 
 const navigationList = [
-	{
-		name: 'content',
-		text: 'Контент',
-		children: [
-			{
-				name: 'job',
-				text: 'Работа',
-				children: [routes.vacancies, routes.candidates],
-			},
-			{
-				name: 'news',
-				text: 'Новости',
-				children: [routes.events],
-			},
-		],
-	},
-	{
-		name: 'users',
-		text: 'Пользователи',
-		children: [
-			{
-				name: 'inner-users',
-				text: 'Внутренние пользователи',
-				children: [routes.clients, routes.partners],
-			},
-		],
-	},
+  {
+    name: 'content',
+    text: 'Контент',
+    children: [
+      {
+        name: 'job',
+        text: 'Работа',
+        children: [routes.vacancies, routes.candidates],
+      },
+      {
+        name: 'news',
+        text: 'Новости',
+        children: [routes.events],
+      },
+    ],
+  },
+  {
+    name: 'users',
+    text: 'Пользователи',
+    children: [
+      {
+        name: 'inner-users',
+        text: 'Внутренние пользователи',
+        children: [routes.clients, routes.partners],
+      },
+    ],
+  },
 ]
 
 // Нужно написать функцию
-const generateNavigationListWithPermissions = (
-	navigationList,
-	checkPermission
-) => {
-	return []
+const generateNavigationListWithPermissions = (navigationList, checkPermission) => {
+  return []
 }
 
 function App() {
-	const navigationListWithPermission = generateNavigationListWithPermissions(
-		navigationList,
-		checkHasUserPermission
-	)
+  const navigationListWithPermission = generateNavigationListWithPermissions(
+    navigationList,
+    checkHasUserPermission
+  )
 
-	return (
-		<div className="container">
-			{navigationListWithPermission.map((item) => item)}
+  return (
+    <div className="container">
+      {navigationListWithPermission.map((item) => item)}
 
-			<div className="navigation">
-				<div className="navigation-level-1">
-					Контент
-					<div className="navigation-level-2">
-						Работа
-						<div className="navigation-level-3">
-							{checkHasUserPermission('vacancies') && <div>Вакансии</div>}
-							{checkHasUserPermission('candidates') && <div>Кандидаты</div>}
-						</div>
-					</div>
-					<div className="navigation-level-2">
-						Новости
-						<div className="navigation-level-3">
-							{checkHasUserPermission('events') && <div>События</div>}
-						</div>
-					</div>
-				</div>
-				<div className="navigation-level-1">
-					Пользователи
-					<div className="navigation-level-2">
-						Клиенты
-						<div className="navigation-level-3">
-							{checkHasUserPermission('clients') && <div>Клиенты</div>}
-							{checkHasUserPermission('partners') && <div>Партнеры</div>}
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	)
+      <div className="navigation">
+        <div className="navigation-level-1">
+          Контент
+          <div className="navigation-level-2">
+            Работа
+            <div className="navigation-level-3">
+              {checkHasUserPermission('vacancies') && <div>Вакансии</div>}
+              {checkHasUserPermission('candidates') && <div>Кандидаты</div>}
+            </div>
+          </div>
+          <div className="navigation-level-2">
+            Новости
+            <div className="navigation-level-3">
+              {checkHasUserPermission('events') && <div>События</div>}
+            </div>
+          </div>
+        </div>
+        <div className="navigation-level-1">
+          Пользователи
+          <div className="navigation-level-2">
+            Клиенты
+            <div className="navigation-level-3">
+              {checkHasUserPermission('clients') && <div>Клиенты</div>}
+              {checkHasUserPermission('partners') && <div>Партнеры</div>}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default App;
+export default App
